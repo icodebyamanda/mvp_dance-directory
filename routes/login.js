9b2e4e471da1ec08dd05c858b90695d7e7af35b1
icodebyamanda <rasoamanda@gmail.com>
2021-02-27 19:22:31 +0100

    Attempting #15 as complementary to #14

--- routes/login.js
+++ routes/login.js
@@ -37,6 +37,8 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/profile", userShouldBeLoggedIn, function (req, res, next) {
+  //db(`SELECT * from classes WHERE userId = ${req.user_id}`) -> advice from Germinal in real life go back on that about why?
+  // db(`SELECT * from instructors WHERE userId = ${req.user_id}`) <- neither do something
   res.send({
     message: "Here is the PROTECTED data for user " + req.user_id,
   });
